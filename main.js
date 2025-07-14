51bace46db3a0454f3323b77b599d5811dfa2962
juegos-baby-mandi <Javi.gacitualiberona@gmail.com>
2025-07-13 22:50:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // JavaScript base
+
+// Hacer scroll suave al hacer clic en los enlaces del menú
+document.querySelectorAll('a[href^="#"]').forEach(ancla => {
+  ancla.addEventListener('click', function (e) {
+    e.preventDefault();
+    const destino = document.querySelector(this.getAttribute('href'));
+    if (destino) {
+      destino.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
